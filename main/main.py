c58d636f1a58a616cd88f064ce0b2007148bbc6f
Krzysiek <krz.gdk@gmail.com>
2015-04-22 10:22:08 +0200

    added scanning links, fixed tables

--- main/main.py
+++ main/main.py
@@ -2,9 +2,12 @@ from tokens.TokenType import TokenType
 from tokens.Token import Token
 
 
+token_list = []
+
+
 def scan_cell(file_string, i):
     cell_content = ""
-    while True:
+    while i < len(file_string):
         if file_string[i] == " " and file_string[i+1] == " " and file_string[i+2] == "|":
             token_list.append(Token(TokenType.tab_right_margin))
             token_list.append(Token(TokenType.tab_normal_sep))
@@ -33,11 +36,12 @@ def scan_cell(file_string, i):
             break
         else:
             cell_content += file_string[i]
+            added_tokens = 0
             i += 1
     if cell_content.strip() == ":::":
-        token_list.insert(-added_tokens, Token(TokenType.cell_merge_symbol))
+        token_list.insert(len(token_list)-added_tokens, Token(TokenType.cell_merge_symbol))
     else:
-        token_list.insert(-added_tokens, cell_content)
+        token_list.insert(len(token_list)-added_tokens, cell_content)
     return i
 
 
@@ -53,7 +57,9 @@ def scan_row(file_string, i):
         i += 2
     i = scan_cell(file_string, i)
     while i < len(file_string) and file_string[i] != "\n":
-        a = file_string[i]
+        if file_string[i] == " " and file_string[i+1] == " ":
+            token_list.append(Token(TokenType.tab_left_margin))
+            i += 2
         i = scan_cell(file_string, i)
     else:
         token_list.append(Token(TokenType.tab_row_end))
@@ -67,11 +73,51 @@ def scan_table(file_string, i):
         i = scan_row(file_string, i)
     return i
 
-token_list = []
+
+def scan_link(file_string, i):
+    token_list.append(Token(TokenType.link_begin))
+    i += 2
+    link_url = ""
+    link_section = ""
+    link_text = ""
+    url_added = False
+    while i < len(file_string):
+        if file_string[i] == "#":
+            token_list.append(Token(TokenType.link_url, link_url))
+            url_added = True
+            token_list.append(Token(TokenType.link_section_sep))
+            i += 1
+            while i < len(file_string) and file_string[i] != "|" and file_string[i:i+1] != "]]":
+                link_section += file_string[i]
+                i += 1
+            token_list.append(Token(TokenType.link_section, link_section))
+        if file_string[i] == "|":
+            if not url_added:
+                token_list.append(Token(TokenType.link_url, link_url))
+                url_added = True
+            token_list.append(Token(TokenType.link_text_sep))
+            i += 1
+            while i < len(file_string):
+                if file_string[i] == "]" and file_string[i+1] == "]":
+                    break
+                link_text += file_string[i]
+                i += 1
+            token_list.append(Token(TokenType.link_text, link_text))
+        if file_string[i] == "]" and file_string[i+1] == "]":
+            if not url_added:
+                token_list.append(Token(TokenType.link_url, link_url))
+                url_added = True
+            token_list.append(Token(TokenType.link_end))
+            i += 2
+            break
+        link_url += file_string[i]
+        i += 1
+    return i
 
 with open('../test.txt', 'r') as file:
     file_string = file.read()
     i = 0
+    content = ""
     while i < len(file_string):
         if file_string[i] == "=":
             if file_string[i + 1] == "=":
@@ -79,43 +125,71 @@ with open('../test.txt', 'r') as file:
                     if file_string[i + 3] == "=":
                         if file_string[i + 4] == "=":
                             if file_string[i + 5] == "=":
+                                token_list.append(Token(TokenType.content, content))
+                                content = ""
                                 token_list.append(Token(TokenType.heading_level6))
                                 i += 6
                                 continue
                             else:
+                                token_list.append(Token(TokenType.content, content))
+                                content = ""
                                 token_list.append(Token(TokenType.heading_level5))
                                 i += 5
                                 continue
                         else:
+                            token_list.append(Token(TokenType.content, content))
+                            content = ""
                             token_list.append(Token(TokenType.TokenType.heading_level4))
                             i += 4
                             continue
                     else:
+                        token_list.append(Token(TokenType.content, content))
+                        content = ""
                         token_list.append(Token(TokenType.heading_level3))
                         i += 3
                         continue
                 else:
+                    token_list.append(Token(TokenType.content, content))
+                    content = ""
                     token_list.append(Token(TokenType.heading_level2))
                     i += 2
                     continue
             else:
+                token_list.append(Token(TokenType.content, content))
+                content = ""
                 token_list.append(Token(TokenType.heading_level1))
                 i += 1
                 continue
         elif file_string[i] == "*" and file_string[i+1] == "*":
+            token_list.append(Token(TokenType.content, content))
+            content = ""
             token_list.append(Token(TokenType.text_bold_symbol))
             i += 2
         elif file_string[i] == "/" and file_string[i+1] == "/":
+            token_list.append(Token(TokenType.content, content))
+            content = ""
             token_list.append(Token(TokenType.text_italics_symbol))
             i += 2
         elif file_string[i] == "_" and file_string[i+1] == "_":
+            token_list.append(Token(TokenType.content, content))
+            content = ""
             token_list.append(Token(TokenType.text_underlined_symbol))
+            i += 2
+        elif file_string[i] == "'" and file_string[i+1] == "'":
+            token_list.append(Token(TokenType.content, content))
+            content = ""
+            token_list.append(Token(TokenType.text_monospaced_symbol))
             i += 2
         elif file_string[i] == "^" or file_string[i] == "|":
             i = scan_table(file_string, i)
+        elif file_string[i] == "[" and file_string[i+1] == "[":
+            i = scan_link(file_string, i)
         else:
-            print(file_string[i])
+            content += file_string[i]
+            #print(file_string[i])
             i += 1
+    token_list.append(Token(TokenType.content, content))
+    content = ""
 
 
 print(token_list)
--- tokens/Token.py
+++ tokens/Token.py
@@ -2,8 +2,9 @@ from tokens.TokenType import TokenType
 
 
 class Token:
-    def __init__(self, token_type):
+    def __init__(self, token_type, content=""):
         self.token_type = token_type
+        self.content = content
 
     def __str__(self):
         if self.token_type == TokenType.heading_level1:
@@ -24,6 +25,8 @@ class Token:
             return "//"
         elif self.token_type == TokenType.text_underlined_symbol:
             return "__"
+        elif self.token_type == TokenType.text_monospaced_symbol:
+            return "''"
         elif self.token_type == TokenType.cell_merge_symbol:
             return ":::"
         elif self.token_type == TokenType.tab_heading_sep:
@@ -33,8 +36,24 @@ class Token:
         elif self.token_type == TokenType.tab_left_margin or self.token_type == TokenType.tab_right_margin:
             return "  "
         elif self.token_type == TokenType.tab_row_end:
-            return "\n"
+            return "ROW_SEP(NEWLINE)"
+        elif self.token_type == TokenType.content:
+            return self.content
+        elif self.token_type == TokenType.link_text:
+            return self.content
+        elif self.token_type == TokenType.link_section:
+            return self.content
+        elif self.token_type == TokenType.link_url:
+            return self.content
+        elif self.token_type == TokenType.link_begin:
+            return "[["
+        elif self.token_type == TokenType.link_section_sep:
+            return "#"
+        elif self.token_type == TokenType.link_text_sep:
+            return "|"
+        elif self.token_type == TokenType.link_end:
+            return "]]"
         else:
-            return "dupa"
+            return "inne"
 
     __repr__ = __str__
